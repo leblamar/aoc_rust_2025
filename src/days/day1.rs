3ca0cb60495d38c809a94e49f4abede9c907e499
Marius Leblanc <marius0leblanc@gmail.com>
2025-12-03 00:22:48 +0100

    Added unit tests

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -11,8 +11,6 @@ impl Day for Day1 {
     fn part1(&self, input: String) -> Result<i64, DayError<'_>> {
         println!("Day 1 part 1");
 
-        let test_input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82".to_string();
-
         let mut dial = 50;
 
         let mut count_0 = 0;
@@ -39,26 +37,6 @@ impl Day for Day1 {
 
     fn part2(&self, input: String) -> Result<i64, DayError<'_>> {
         println!("Day 1 part 2");
-
-        //let test_input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82".to_string(); //6
-        //let test_input = "L50\nL100".to_string(); //2
-        //let test_input = "L50\nL1".to_string(); //1
-        //let test_input = "L50\nR50".to_string(); //1
-        //let test_input = "L50\nR101".to_string(); //2
-        //let test_input = "R50\nR100".to_string(); //2
-        //let test_input = "R50\nR1".to_string(); //1
-        //let test_input = "L100\nR151".to_string(); //3
-        //let test_input = "L7\nL743".to_string(); //8
-        //let test_input = "L7\nL143".to_string(); //2
-        //let test_input = "L7\nL142".to_string(); //1
-        //let test_input = "R7\nR143".to_string(); //2
-        //let test_input = "L57\nL93".to_string(); //2
-        //let test_input = "L57\nL193".to_string(); //3
-        //let test_input = "L57\nL192".to_string(); //2
-        //let test_input = "L57\nR107".to_string(); //3
-        //let test_input = "L57\nR108".to_string(); //3
-        //let test_input = "L57\nR106".to_string(); //2
-        //let test_input = "L57\nR207".to_string(); //4
 
         let mut dial = 50;
 
@@ -96,5 +74,171 @@ impl Day for Day1 {
         }
 
         Ok(count_0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::days::day1::Day1;
+    use crate::utils::day::Day;
+
+    #[test]
+    fn it_test_example_part1() {
+        let test_input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82".to_string();
+        let result = Day1.part1(test_input).expect("There should be a result");
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn it_test_example_part2() {
+        let test_input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn it_test_double_exact_sub() {
+        let test_input = "L50\nL100".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+    
+    #[test]
+    fn it_test_double_sub() {
+        let test_input = "L50\nL1".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn it_test_sub_add() {
+        let test_input = "L50\nR50".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn it_test_sub_big_add() {
+        let test_input = "L50\nR101".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_double_add() {
+        let test_input = "R50\nR100".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_double_small_add() {
+        let test_input = "R50\nR1".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn it_test_sub_add_big() {
+        let test_input = "L100\nR151".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn it_test_double_sub_big() {
+        let test_input = "L7\nL743".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn it_test_double_sub_big_2() {
+        let test_input = "L7\nL143".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_double_sub_big_3() {
+        let test_input = "L7\nL142".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn it_test_double_add_big() {
+        let test_input = "R7\nR143".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_double_sub_2() {
+        let test_input = "L57\nL93".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_double_sub_big_4() {
+        let test_input = "L57\nL193".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn it_test_double_sub_big_5() {
+        let test_input = "L57\nL192".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_sub_add_big_2() {
+        let test_input = "L57\nR107".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn it_test_sub_add_big_3() {
+        let test_input = "L57\nR108".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn it_test_sub_add_big_4() {
+        let test_input = "L57\nR106".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn it_test_sub_add_big_5() {
+        let test_input = "L57\nR207".to_string();
+        let result = Day1.part2(test_input).expect("There should be a result");
+
+        assert_eq!(result, 4);
     }
 }
